21388337af3a1d83ccd53cc90d2f5df20e26cb65
Claude BUENO <claude.bueno@outlook.com>
2016-06-22 10:45:57 +0200

    Foster > Icons Réseaux sociaux
    // Todo : transformer en Widget

--- footer.php
+++ footer.php
@@ -17,6 +17,18 @@
 
 	<footer class="site-footer" role="contentinfo">
 		<div class="clearfix container">
+			<div class="row">
+				<div class="col-sm-12 site-info">
+					<center>
+						<a href="https://twitter.com/claudebueno" title="Follow me on Twitter" target="_blank"><i class="fa fa-twitter" style="font-size:24px;color:#FFFFFF; margin-left:10px; margin-right:10;"></i></a>
+						<a href="https://www.facebook.com/claude.bueno" title="Follow me on Facebook" target="_blank"><i class="fa fa-facebook" style="font-size:24px;color:#FFFFFF;margin-left:10px; margin-right:10;"></i></a>
+						<a href="https://www.linkedin.com/in/claudebueno" title="Follow me on LinkedIn" target="_blank"><i class="fa fa-linkedin" style="font-size:24px;color:#FFFFFF;margin-left:10px; margin-right:10;"></i></a>
+						<a href="https://plus.google.com/+ClaudeBUENO/" title="Follow me on Google+" target="_blank"><i class="fa fa-google-plus" style="font-size:24px;color:#FFFFFF;margin-left:10px; margin-right:10;"></i></a>
+						<a href="https://github.com/claudebueno" title="Follow me on GitHub" target="_blank"><i class="fa fa-github" style="font-size:24px;color:#FFFFFF;margin-left:10px; margin-right:10;"></i></a>
+						<a href="https://bitbucket.org/claudebueno/" title="Follow me on Bitbucket" target="_blank"><i class="fa fa-bitbucket" style="font-size:24px;color:#FFFFFF;margin-left:10px; margin-right:10;"></i></a>
+					</center>
+				</div>
+		</div>
 				<div class="row">
 					<div class="col-sm-6 site-info">
 						&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. <?php _e( 'All rights reserved', 'pointb' ); ?>.
